a84b98bd654ab0657758a0ba0da72af7444cd7ec
Steven Koska <stevenkoska2000@gmail.com>
2019-11-22 09:11:09 +0200

    2nd commit

---
+++ OdeToFood/OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var controller = RouteData.Values["controller"];
+            var action = RouteData.Values["action"];
+            var id = RouteData.Values["id"];
+
+            var message = string.Format("{0}::{1} {2}", controller, action, id);
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var Model = new AboutModel();
+            Model.Name = "Steven";
+            Model.Location = "Tallinn, Estonia";
+            Model.Age = 19;
+
+            return View(Model);
+
+            
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
